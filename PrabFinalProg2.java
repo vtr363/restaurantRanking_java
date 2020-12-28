c3ad7a07367bf62ce1bf175b13a0683c3ac65e85
Victor Miguel <70338725+vtr363@users.noreply.github.com>
2020-12-28 14:10:20 -0300

    Add files via upload

---
+++ PrabFinalProg2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prab.pkgfinal.prog.pkg2;
+
+/**
+ *
+ * @author victor m
+ */
+public class PrabFinalProg2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
